4986ea55c42d3e94938e251765abda7589eaeee5
Isabel Lee <isabelringing1@gmail.com>
2020-10-05 14:11:56 -0400

    Now loads all images from specified folder in browser.

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ var path = require("path");
 
 const app = express();
 
-var BoxSDK = require('box-node-sdk');
-var folder_id = '123366833733'
-var sdk = new BoxSDK({
-    clientID: 'p22qhwh72omj1kk8ifbs00tusjkz1il0',
-    clientSecret: 'O1aFGAaywW8ShSgGnWCqxFkJLKJpvtP3'
-  });
-var client = sdk.getBasicClient('WzOybhHQQNkldl9hVrc1kkkngkEvFim9');
-
 app.use(express.static(__dirname + "/"));
 
 app.get('/', (req, res) => {
@@ -26,39 +18,3 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Senior Project app listening on port ${port}`);
 });
-
-client.get('/folders/' + folder_id + '/items', {qs: {fields: 'id,name'}})
-	.then(response => { res.send(response)/* ... */ })
-	.catch(error => { /* handle any errors */ });
-
-/*
-client.folders.getItems(
-    folder_id,
-    {
-        usemarker: 'false',
-        fields: 'name',
-        offset: 0,
-        limit: 100
-    })
-    .then(items => {
-        
-        /* items -> {
-            total_count: 2,
-            entries: 
-            [ { type: 'folder',
-                id: '11111',
-                sequence_id: '1',
-                etag: '1',
-                name: 'Personal Documents' },
-                { type: 'file',
-                id: '22222',
-                sequence_id: '0',
-                etag: '0',
-                name: 'Q2 Strategy.pptx' } ],
-            offset: 0,
-            limit: 25,
-            order: 
-            [ { by: 'type', direction: 'ASC' },
-                { by: 'name', direction: 'ASC' } ] }
-        
-    });*/
--- js/train.js
+++ js/train.js
@@ -1,7 +1,55 @@
 document.addEventListener('DOMContentLoaded', setup);
 
+var userToken = '';;
+
 function setup(){
-    $.get('https://isabel-senior-project.herokuapp.com/folders/123366833733/items', function(info) {
-        console.log(info);
-     });
+    if (!sessionStorage.getItem('userToken')){ 
+        userToken = prompt("Please enter the generated developer token.");
+        sessionStorage.setItem('userToken', userToken);
+    }
+    userToken = sessionStorage.getItem('userToken');
+    $.ajax('https://api.box.com/2.0/folders/123366833733/items', 
+    {
+        type: 'GET',
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + userToken);
+        },
+        success: function(result) {
+            console.log(result);
+            processImages(result.entries);
+        },
+        error: function(xhr, status, error) { 
+            alert("Something went wrong, trying again.");
+            sessionStorage.removeItem('userToken');
+            setup();
+         }
+    })
+}
+
+function getImage(id){
+    $.ajax('https://api.box.com/2.0/files/' + id + '/content?access_token=' + userToken, 
+    {
+        type: 'GET',
+        success: function(data, status, xhr) {
+            var img = document.createElement('img');            
+            var url = window.URL || window.webkitURL;
+            img.src = url.createObjectURL(data);
+            img.style.width = '50vw'; 
+            document.body.appendChild(img);
+        },
+        xhr:function(){
+            var xhr = new XMLHttpRequest();
+            xhr.responseType = 'blob'
+            return xhr;
+        },
+        error: function(xhr, status, error) { 
+            console.log(xhr);
+         }
+    })
+}
+
+function processImages(images){
+    for (i in images){
+        getImage(images[i].id);
+    }
 }
